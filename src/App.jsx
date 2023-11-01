f33724fa6c2159b96a5c976f96ef832168f9c5b3
Victor Golovin <rinoally@gmail.com>
2023-11-01 04:44:25 +0300

    add desux store and action

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useSelector } from "react-redux"
+
+const getCounterSelector = state => state.value; // Selector нам нужен чтобы в себя принимать state и из этого state возвращать value (данные)
+
+
+
 const App = () => {
+  const counter = useSelector(getCounterSelector); // useSelector() позволяет нам получить информацию из store, но внутри него нам нужно передать функцию getCounterSelector
+
+
   return (
     <div>
       <p>
-        Hello Redux!
+        {counter}
       </p>
     </div>
   )
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import { counterReducer } from './reducer.js'
+import { Provider } from 'react-redux'
 import './index.css'
+
+
+
+
+const store = createStore(counterReducer) // В createStore прокидываем наш reducer
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<App />)
+root.render(  // <Provider> Позволяет нам связывать компоненты react с store redux (сам Provider находится в react-redux библиотеке)
+    <React.StrictMode>
+        <Provider store={store}> 
+            <App />
+        </Provider>
+    </React.StrictMode>
+)
